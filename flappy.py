dd3334dad0a76cde705480315e3a26d49ac9f411
trmlnm <filledinanxiety@gmail.com>
2020-11-29 00:46:57 +0100

    add moving floor to screen

--- flappy.py
+++ flappy.py
@@ -1,17 +1,24 @@
 import sys
 import pygame
 
-
-pygame.init()
-
-
+# Constants for screen width and height
 SCREEN_H = 512
 SCREEN_W = 288
+
+
+def draw_floor():
+    screen.blit(floor_surface, (floor_x_pos, 450))
+    screen.blit(floor_surface, (floor_x_pos + SCREEN_W, 450))
+
+
+pygame.init()
 
 screen = pygame.display.set_mode((SCREEN_W, SCREEN_H))
 clock = pygame.time.Clock()
 
 bg_surface = pygame.image.load('assets/background-day.png').convert()
+floor_surface = pygame.image.load('assets/base.png').convert()
+floor_x_pos = 0
 
 while True:
     for event in pygame.event.get():
@@ -22,6 +29,10 @@ while True:
             sys.exit()
 
     screen.blit(bg_surface, (0, 0))
+    floor_x_pos -= 1
+    draw_floor()
+    if floor_x_pos <= -SCREEN_W:
+        floor_x_pos = 0
 
     pygame.display.update()
     clock.tick(120)
