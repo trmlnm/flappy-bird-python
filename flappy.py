55d28ec3853efcf94ccfec8237a2ff81e20cb7d9
trmlnm <filledinanxiety@gmail.com>
2020-11-29 00:00:20 +0100

    add assets and background image to screen

--- flappy.py
+++ flappy.py
@@ -11,6 +11,7 @@ SCREEN_W = 288
 screen = pygame.display.set_mode((SCREEN_W, SCREEN_H))
 clock = pygame.time.Clock()
 
+bg_surface = pygame.image.load('assets/background-day.png').convert()
 
 while True:
     for event in pygame.event.get():
@@ -19,6 +20,8 @@ while True:
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
+
+    screen.blit(bg_surface, (0, 0))
 
     pygame.display.update()
     clock.tick(120)
