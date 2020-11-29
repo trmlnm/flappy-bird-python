28fdb97b61e3ab69cbcbb60956b4c96b0476bac4
trmlnm <filledinanxiety@gmail.com>
2020-11-29 21:16:56 +0100

    add bird image and movement

--- flappy.py
+++ flappy.py
@@ -16,9 +16,17 @@ pygame.init()
 screen = pygame.display.set_mode((SCREEN_W, SCREEN_H))
 clock = pygame.time.Clock()
 
+# Game Variables
+
+gravity = 0.125
+bird_movement = 0
+
 bg_surface = pygame.image.load('assets/background-day.png').convert()
 floor_surface = pygame.image.load('assets/base.png').convert()
 floor_x_pos = 0
+
+bird_surface = pygame.image.load('assets/bluebird-midflap.png').convert()
+bird_rect = bird_surface.get_rect(center=(50, 256))
 
 while True:
     for event in pygame.event.get():
@@ -27,8 +35,17 @@ while True:
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_SPACE:
+                print("flap")
+                bird_movement = 0
+                bird_movement -= 6
 
     screen.blit(bg_surface, (0, 0))
+
+    bird_movement += gravity
+    bird_rect.centery += bird_movement
+    screen.blit(bird_surface, bird_rect)
     floor_x_pos -= 1
     draw_floor()
     if floor_x_pos <= -SCREEN_W:
