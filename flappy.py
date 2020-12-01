efcd1e814dab4a0d63ee91064f0321e541c42c6c
trmlnm <filledinanxiety@gmail.com>
2020-12-01 14:02:18 +0100

    add collision detection, stops the game when collision happen and allow to restart it

--- flappy.py
+++ flappy.py
@@ -34,6 +34,17 @@ def draw_pipes(pipes):
             screen.blit(flip_pipe, pipe)
 
 
+def check_collision(pipes):
+    for pipe in pipes:
+        if bird_rect.colliderect(pipe):
+            return False
+
+    if bird_rect.top <= -50 or bird_rect.bottom >= 450:
+        return False
+
+    return True
+
+
 pygame.init()
 
 screen = pygame.display.set_mode((SCREEN_W, SCREEN_H))
@@ -43,6 +54,8 @@ clock = pygame.time.Clock()
 
 gravity = 0.125
 bird_movement = 0
+
+game_active = True
 
 bg_surface = pygame.image.load('assets/background-day.png').convert()
 floor_surface = pygame.image.load('assets/base.png').convert()
@@ -66,10 +79,16 @@ while True:
             sys.exit()
 
         if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_SPACE:
+            if event.key == pygame.K_SPACE and game_active:
                 # print("flap")
                 bird_movement = 0
                 bird_movement -= 6
+
+            if event.key == pygame.K_SPACE and game_active == False:
+                pipe_list.clear()
+                bird_rect.center = (50, 256)
+                bird_movement = 0
+                game_active = True
 
         if event.type == SPAWNPIPE:
             # print("pipe")
@@ -77,14 +96,16 @@ while True:
 
     screen.blit(bg_surface, (0, 0))
 
-    # Bird
-    bird_movement += gravity
-    bird_rect.centery += bird_movement
-    screen.blit(bird_surface, bird_rect)
+    if game_active:
+        # Bird
+        bird_movement += gravity
+        bird_rect.centery += bird_movement
+        screen.blit(bird_surface, bird_rect)
+        game_active = check_collision(pipe_list)
 
-    # Pipes
-    pipe_list = move_pipes(pipe_list)
-    draw_pipes(pipe_list)
+        # Pipes
+        pipe_list = move_pipes(pipe_list)
+        draw_pipes(pipe_list)
 
     # Floor
     floor_x_pos -= 1
